5650028b45cfa491733423f334ea1e8702926608
Osama Hussein <osamahusseinn2@gmail.com>
2025-05-10 11:07:51 -0400

    Set up image views

--- Vulkan Practice/Main.cpp
+++ Vulkan Practice/Main.cpp
@@ -105,6 +105,8 @@ private:
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
 
+    std::vector<VkImageView> swapChainImageViews;
+
     void createInstance()
     {
         if (enableValidationLayers && !checkValidationLayerSupport()) {
@@ -415,6 +417,46 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
+    }
+
+    void createImageViews() 
+    {
+        swapChainImageViews.resize(swapChainImages.size());
+
+        for (size_t i = 0; i < swapChainImages.size(); i++) 
+        {
+            // The parameters for image view creation are specified in a VkImageViewCreateInfo structure
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+
+            /* The viewType and format fields specify how the image data should be interpreted. The viewType parameter allows
+            you to treat images as 1D textures, 2D textures, 3D textures and cube maps */
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            /* The components field allows you to swizzle the color channels around. For example, you can map all of the
+            channels to the red channel for a monochrome texture. You can also map constant values of 0 and 1 to a channel.
+            In our case we'll stick to the default mapping */
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            /* The subresourceRange field describes what the image's purpose is and which part of the image should be
+            accessed. Our images will be used as color targets without any mipmapping levels or multiple layers */
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) 
+            {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
     }
 
     void createSwapChain() 
@@ -644,6 +686,11 @@ private:
 
     void cleanup() 
     {
+        for (auto imageView : swapChainImageViews) 
+        {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
 
